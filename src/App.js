23a4f03961cb8a19155ce546c8ed10c33bc7963d
Sinthika2000 <sinthikamalshani01@gmail.com>
2024-03-19 08:36:30 +0530

    create theme and app bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseLine, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+
 function App() {
-  return 
-    <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseLine />
+    <div className="app">
+      <main className="context">
+        <Topbar />
+      </main>
+    </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
